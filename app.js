2c52a3e04a167aee7a39dd63066ae249beb22228
chris_chan <chrischandfw@gmail.com>
2022-03-08 14:49:30 -0600

    added dragon image and grabbed dom elements

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
+const hatImg0 = document.getElementById('hat-0');
+const hatImg1 = document.getElementById('hat-1');
+const hatImg2 = document.getElementById('hat-2');
 
+const button0 = document.getElementById('button-0');
+const button1 = document.getElementById('button-1');
+const button2 = document.getElementById('button-2');
+
+const winsEl = document.getElementById('wins');
+const totalEl = document.getElementById('total');
 // let state
-
+let wins = 0;
+let total = 0:
 // set event listeners 
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
