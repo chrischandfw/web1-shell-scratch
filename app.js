130ddccdadc5220734c09409d4041959ed09d22e
chris_chan <chrischandfw@gmail.com>
2022-03-10 00:11:57 -0600

    created function to spin pearl

--- app.js
+++ app.js
@@ -76,7 +76,8 @@ document.querySelector('.rotate').addEventListener('click', function(){
     current_rotation += 90;
     document.querySelector('.rotate').style.transform = 'rotate(' + current_rotation + 'deg)';
 });
-const current_rotation = 0;
+let current_rotation = 0;
+
 
   // get user input
   // use user input to update state 
